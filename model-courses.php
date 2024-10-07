3d1d1ee5d7b6823ff6f6a3bf6c711f197df3156d
ClaireYarber <clyarber@ou.edu>
2024-10-07 13:13:31 -0500

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -1,15 +1,15 @@
-‹?php
-function selectInstructors() {
-  try {
-    $conn = get_db_connection();
-    $stmt = $conn->prepare("SELECT instructor_id, instructor_name, office number FROM 'instructor'");
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $conn->close();
-    return $result;
-} catch (Exception $e) {
-    $conn->close();
-    throw $e;
-  }
+<?php
+function selectCourses() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT course_id, course_name, credits FROM `courses`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
 }
 ?>
