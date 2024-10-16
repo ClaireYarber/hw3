e09d1eca7b0290110bd9e23877f4b68197878b33
ClaireYarber <clyarber@ou.edu>
2024-10-16 13:12:48 -0500

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -1,24 +1,25 @@
-<h1>Courses</h1>h1>
+<h1>Courses</h1>
 <div class="table-responsive">
   <table class="table">
-    <thread>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Credits</th>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Credits</th>
       </tr>
-    </thread>
+    </thead>
     <tbody>
   <?php
   while ($course = $courses->fetch_assoc()) {
   ?>
     <tr>
-      <td><?php echo $course{'course_id']; ?></td>
-      <td><?php echo $course{'course_name']; ?></td>
-      <td><?php echo $course{'credits']; ?></td>
+      <td><?php echo $course['course_id']; ?></td>
+      <td><?php echo $course['course_name']; ?></td>
+      <td><?php echo $course['credits']; ?></td>
     </tr>
   <?php
   }
   ?>
-      </tbody>
-    </table>
-  </div>
+    </tbody>
+  </table>
+</div>
